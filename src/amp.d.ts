3eaef824bd69c0bcbb6e1076db9417ee3f3e6fee
[dyad] <brunobg87@gmail.com>
2025-11-27 13:18:14 -0300

    [dyad] Corrigindo erros de tipo do AMP via tsconfig - wrote 2 file(s) + extra files edited outside of Dyad

--- src/amp.d.ts
+++ src/amp.d.ts
@@ -1,18 +1,15 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import React from 'react';
 
-// Garante que este arquivo seja tratado como um módulo para permitir declare global
-export {};
+// Este arquivo aumenta o namespace global JSX para incluir elementos AMP.
+// É intencionalmente mantido simples, sem importações/exportações, para garantir que seja tratado como um script global.
 
-declare global {
-  namespace JSX {
-    interface IntrinsicElements {
-      'amp-story': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & { [key: string]: any };
-      'amp-story-page': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & { [key: string]: any };
-      'amp-story-grid-layer': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & { [key: string]: any };
-      'amp-img': React.DetailedHTMLProps<React.ImgHTMLAttributes<HTMLImageElement>, HTMLImageElement> & { [key: string]: any };
-      'amp-story-bookend': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & { [key: string]: any };
-      'amp-video': React.DetailedHTMLProps<React.VideoHTMLAttributes<HTMLVideoElement>, HTMLVideoElement> & { [key: string]: any };
-    }
+declare namespace JSX {
+  interface IntrinsicElements {
+    'amp-story': any;
+    'amp-story-page': any;
+    'amp-story-grid-layer': any;
+    'amp-img': any;
+    'amp-story-bookend': any;
+    'amp-video': any;
   }
 }
