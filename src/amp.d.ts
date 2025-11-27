b6d7f70b1ebfda41ed3ab05a2b7c4b69ab68529e
[dyad] <brunobg87@gmail.com>
2025-11-27 13:15:42 -0300

    [dyad] Fixing AMP TypeScript errors - wrote 1 file(s) + extra files edited outside of Dyad

--- src/amp.d.ts
+++ src/amp.d.ts
@@ -1,15 +1,15 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import React from 'react';
 
-declare global {
-  namespace JSX {
-    interface IntrinsicElements {
-      'amp-story': any;
-      'amp-story-page': any;
-      'amp-story-grid-layer': any;
-      'amp-img': any;
-      'amp-story-bookend': any;
-      'amp-video': any;
-    }
+// IntrinsicElements are looked up on the global JSX namespace.
+// We avoid top-level imports here to ensure this file is treated as a script/global declaration.
+
+declare namespace JSX {
+  interface IntrinsicElements {
+    'amp-story': any;
+    'amp-story-page': any;
+    'amp-story-grid-layer': any;
+    'amp-img': any;
+    'amp-story-bookend': any;
+    'amp-video': any;
   }
 }
